8610c651eac9a489f98d31a0e761f07b2df7b457
Mycaelleoliveira <112009466+Mycaelleoliveira@users.noreply.github.com>
2022-10-18 23:16:55 -0300

    Add files via upload

---
+++ unidade3/aula9php/ex3.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="" method="get">
+    <p>3.Ler 3 valores (considere que não serão informados valores iguais) e escrever a soma dos 2 maiores.</p>
+    Primeiro valor:<input type="number" name="um"><br /><br />
+    Segundo valor:<input type="number" name="dois"><br /><br />
+    Terceiro valor:<input type="number" name="tres"><br /><br />
+    <input type="submit" value="Enviar">
+  </form>
+  <?php
+  $um= $_GET["um"];
+  $dois= $_GET["dois"];
+  $tres= $_GET["tres"];
+  $soma = 0;
+
+  if ($um > $dois && $um > $tres) {
+    $soma = $soma + $um;
+
+    if ($dois > $tres) {
+      $soma = $soma + $dois;
+    } else {
+      $soma = $soma + $tres;
+    }
+  } elseif ($dois > $um && $dois > $tres) {
+    $soma = $soma + $dois;
+
+    if ($um > $tres) {
+      $soma = $soma + $um;
+    } else {
+      $soma = $soma + $tres;
+    }
+  } else {
+    $soma = $soma + $tres;
+
+    if ($um > $dois) {
+      $soma = $soma + $um;
+    } else {
+      $soma = $soma + $tres;
+    }
+  }
+
+  echo "Soma: " . $soma;
+  ?>
+</body>
+
+</html>
---
+++ unidade3/aula9php/teste.php
@@ -0,0 +1,6 @@
+<?php
+$nome = "Mycaelle";
+
+for ($i = 0; $i < 8; $i++) {
+  echo $nome[$i] . "<br />";
+}
